64291269e90b797953ceecf0c6eba9004cef5e4c
2696212633@qq.com <250393_sm>
2019-02-12 18:34:29 +0800

    Initial commit

---
+++ ruoyi-admin/src/main/java/com/ruoyi/RuoYiApplication.java
@@ -0,0 +1,23 @@
+package com.ruoyi;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+/**
+ * 启动程序
+ *
+ * @author ruoyi
+ */
+@Slf4j
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+public class RuoYiApplication {
+
+    public static void main(String[] args) {
+        log.info("系统启动==========》begin");
+        SpringApplication.run(RuoYiApplication.class, args);
+        log.info("系统启动==========》end");
+    }
+
+}
